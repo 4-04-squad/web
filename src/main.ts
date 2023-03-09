f721dec0002f1f67b26149e89053351dfd1709af
MarJC5 <admin@marjc.ch>
2023-03-09 17:07:05 +0000

    Fix: base api url

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,15 @@ import piniaPersist from "pinia-plugin-persist";
 
 import App from "./App.vue";
 import router from "./router";
+import axios from 'axios'
 
 import "./assets/stylesheets/main.scss";
 
 const app = createApp(App);
 const pinia = createPinia();
+
+// Set the base URL for Axios requests
+axios.defaults.baseURL = '/api'
 
 pinia.use(piniaPersist);
 app.use(pinia);
